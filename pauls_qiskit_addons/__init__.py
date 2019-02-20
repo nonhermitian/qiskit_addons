15745230403b9c52e40f202f494e4ffe506e7419
Paul Nation <nonhermitian@gmail.com>
2019-02-20 11:30:06 -0500

    add to import *

--- pauls_qiskit_addons/__init__.py
+++ pauls_qiskit_addons/__init__.py
@@ -3,3 +3,4 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
+from pauls_qiskit_addons.backends.abstract_backend import AbstractBackend
