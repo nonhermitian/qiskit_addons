6b74be53536ece55e3ace08dcdf458dee0dcd48e
Paul Nation <nonhermitian@gmail.com>
2019-02-20 12:26:35 -0500

    add version info

--- pauls_qiskit_addons/__init__.py
+++ pauls_qiskit_addons/__init__.py
@@ -3,4 +3,6 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
+from pauls_qiskit_addons.version import version as __version__
+
 from pauls_qiskit_addons.backends.abstract_backend import AbstractBackend
